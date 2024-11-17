b1960f1740f4c8f8775c7df3ca3e9c41502ffbca
FadliBilal <fadlibilal783@gmail.com>
2024-11-18 01:20:32 +0700

    update

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,37 +1,27 @@
-//jquery-click-scroll
-//by syamsul'isul' Arifin
-
-var sectionArray = [1, 2, 3, 4, 5];
-
-$.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 75;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 75;
+$(document).ready(function () {
+    var sectionArray = [1, 2, 3, 4, 5];
+  
+    // Highlight active link saat scrolling
+    $.each(sectionArray, function (index, value) {
+      $(document).on('scroll', function () {
+        var offsetSection = $('#section_' + value).offset().top - $('.navbar').height() - 5;
+        var docScroll = $(document).scrollTop();
+        if (docScroll >= offsetSection) {
+          $('.navbar-nav .nav-link').removeClass('active').addClass('inactive');
+          $('.navbar-nav .nav-link').eq(index).addClass('active').removeClass('inactive');
+        }
+      });
+  
+      // Scroll ke bagian saat link diklik
+      $('.click-scroll').eq(index).on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+        var offsetClick = $('#section_' + value).offset().top - $('.navbar').height();
+        $('html, body').animate({ scrollTop: offsetClick }, 300);
+      });
     });
-    
-});
-
-$(document).ready(function(){
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
-    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
-    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
-});
+  
+    // Atur status awal untuk link pertama
+    $('.navbar-nav .nav-link').removeClass('active').addClass('inactive');
+    $('.navbar-nav .nav-link').eq(0).addClass('active').removeClass('inactive');
+  });
+  
--- js/custom.js
+++ js/custom.js
@@ -1,75 +1,60 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
+  // Tutup navbar collapse saat link diklik
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });  
+
+  // Smooth scrolling
+  $('.smoothscroll').on('click', function (e) {
+    e.preventDefault();
+    var target = $(this.hash);
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - $('.navbar').height()
+      }, 300);
+    }
+  });
+
+  // Highlight active link saat scrolling
+  $(window).on('scroll', function () {
+    var scrollPos = $(document).scrollTop();
+    $('.smoothscroll').each(function () {
+      var target = $(this.hash);
+      if (target.length) {
+        var topOffset = target.offset().top - $('.navbar').height() - 5;
+        var bottomOffset = topOffset + target.outerHeight();
+        if (scrollPos >= topOffset && scrollPos < bottomOffset) {
+          $('.smoothscroll').removeClass('active');
+          $(this).addClass('active');
+        }
       }
     });
+  });
 
-    $(window).on('scroll', function(){
-      function isScrollIntoView(elem, index) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(window).height()*.5;
-        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
-          $(elem).addClass('active');
-        }
-        if(!(elemBottom <= docViewBottom)) {
-          $(elem).removeClass('active');
-        }
-        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
-        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
-        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
-      }
-      var timeline = $('#vertical-scrollable-timeline li');
-      Array.from(timeline).forEach(isScrollIntoView);
-    });
-  
-  })(window.jQuery);
+})(window.jQuery);
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const image = document.querySelector(".topics-detail-block-image");
-    const body = document.body;
-  
-    // Buat elemen modal
+// Modal untuk gambar zoomable
+document.addEventListener("DOMContentLoaded", function () {
+  const image = document.querySelector(".topics-detail-block-image");
+  if (image) {
     const modal = document.createElement("div");
     modal.classList.add("image-modal");
     modal.innerHTML = '<img src="" alt="Zoomed Image">';
-    body.appendChild(modal);
-  
+    document.body.appendChild(modal);
+
     const modalImage = modal.querySelector("img");
-  
-    // Ketika gambar diklik
+
+    // Buka modal saat gambar diklik
     image.addEventListener("click", function () {
-      modalImage.src = image.src; // Ambil sumber gambar
+      modalImage.src = image.src;
       modal.classList.add("active");
     });
-  
-    // Tutup modal ketika overlay diklik
+
+    // Tutup modal saat overlay diklik
     modal.addEventListener("click", function () {
       modal.classList.remove("active");
     });
-  });
+  }
+});
